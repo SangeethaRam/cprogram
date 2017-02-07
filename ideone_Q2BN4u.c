f21ebdfce0c48693a6f2f1a284314f025c875710
SangeethaRam <1316116@saec.ac.in>
2017-02-07 14:57:43 +0530

    Add files via upload

---
+++ ideone_Q2BN4u.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int year;
+	scanf("%d",&year);
+	if(year%4==0)
+	printf("\n leap year");
+	else
+	printf("\n not leap year");
+	return 0;
+}
