fd645a11cec6761834c04aa677b1d46a19098d2f
SangeethaRam <1316116@saec.ac.in>
2017-02-07 14:31:38 +0530

    Add files via upload

---
+++ ideone_crUs2N.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	if(n>0)
+	printf("\n negative");
+	if(n>0)
+	printf("\n positive");
+	else
+	printf("\n zero");
+	return 0;
+}
